cd64cdc90ea5ed6dcb838d0499f481db0bfec070
Pooja Rao <raopooj@gmail.com>
2021-08-25 14:38:55 +1000

    Practice express codes

---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports.users = [
+    {
+        'firstname': "James",
+        'lastname': "Bond",
+        'email': "james.bond@gmail.com",
+        'password': "b6b7fb4cad4bc020f76e16889a8e9065cb708d0f8c304a8a3db609b644da9536"  
+    },
+    {
+        'firstname': "Tony",
+        'lastname': "Stark",
+        'email': "starkrulz@gmail.com",
+        'password': "a836ebba36776b21dd0f5cdca497bff65c5bdfc8411cfbfe0111f27bde1c1894"  
+    },
+    {
+        'firstname': "Ali",
+        'lastname': "G",
+        'email': "nameisnotborat@gmail.com",
+        'password': "3b5fe14857124335bb8832cc602f8edcfa12db42be36b135bef5bca47e3f2b9c"  
+    }
+]
+
+module.exports.posts = [
+    {
+        'user_id': 0,
+        'post': "Here is my first post"
+    },
+    {
+        'user_id': 0,
+        'post': "Today I bought a coffee, lockdown sucks."
+    },
+    {
+        'user_id': 0,
+        'post': "This is probably my last post, this blog site is not very good."
+    },
+    {
+        'user_id': 2,
+        'post': "Had leftover pizza for breakfast."
+    }
+]
---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const bcrypt = require('bcryptjs');
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const data = require('./data')
+//const {users,posts} = require('./data')-can be done this way too
+
+//Set view engine as EJS
+app.set('view engine', 'ejs')
+//Set our static folder
+app.use(express.static('public'))
+
+//Body Parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+//Home page
+app.get('/',(req,res) =>{
+    res.render('index',{
+        name:"Pooja"
+    })
+})
+//Display all users
+app.get('/users',(req,res) =>{
+    res.json(data.users)
+})
+//Add a new user
+app.post('/users', (req,res) => {
+    const {firstname, lastname, email, password} = req.body
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    const newUser = {
+        firstname,
+        lastname,
+        email,
+        password:hash
+    }
+    data.users.push(newUser)
+    res.json(data.users)
+})
+
+//Display a single user
+app.get('/posts/:id',(req,res) =>{
+    const found = data.posts.some(post => 
+    post.id === Number(req.params.id))
+    if(found){
+     res.send(data.posts[req.params.id])//use this for project
+    //  Other way of doing the same
+    //  const post = data.posts.filter(post => post.id === Number(req.params.id))
+    //  res.send(post[0])
+    }else{
+        res.send('Post not found')
+    }
+    //res.send(req.params.id)
+})
+
+app.listen(PORT, () => {
+    console.log('App is listening at http://localhost:3000')
+})
